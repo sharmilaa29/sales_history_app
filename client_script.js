698e8f2a1e4ba860b26005f0dbe2d991c65573df
sharmilaa29 <sharmila2018m@gmail.com>
2025-03-20 21:25:29 +0530

    Commit on html table
    Changed the table format from frappe table to html table

--- client_script.js
+++ client_script.js
@@ -8,79 +8,13 @@ frappe.ui.form.on('Sales Order Item', {
             return;
         }
 
-        var selling_details = [];
-
         const dialog = new frappe.ui.Dialog({
             title: __('Selling History'),
             size: 'extra-large',
             fields: [
                 {
-                    fieldtype: 'Table',
-                    fieldname: 'selling_details',
-                    fields: [
-                        {
-                            fieldtype: 'Link',
-                            fieldname: 'invoice_no',
-                            label: __('Inv No'),
-                            options: 'Sales Invoice',
-                            in_list_view: 1,
-                            columns: 2,
-                            read_only: 1
-                        },
-                        {
-                            fieldtype: 'Date',
-                            fieldname: 'invoice_date',
-                            label: __('Date'),
-                            in_list_view: 1,
-                            columns: 2,
-                            read_only: 1
-                        },
-                        {
-                            fieldtype: 'Link',
-                            fieldname: 'customer_code',
-                            label: __('Customer'),
-                            options: 'Customer',
-                            in_list_view: 1,
-                            columns: 2,
-                            read_only: 1
-                        },
-                        {
-                            fieldtype: 'Currency',
-                            fieldname: 'rate',
-                            label: __('Rate'),
-                            in_list_view: 1,
-                            columns: 1,
-                            read_only: 1
-                        },
-                        {
-                            fieldtype: 'Float',
-                            fieldname: 'qty',
-                            label: __('Qty'),
-                            in_list_view: 1,
-                            columns: 1,
-                            read_only: 1
-                        },
-                        {
-                            fieldtype: 'Currency',
-                            fieldname: 'amount',
-                            label: __('Amount'),
-                            in_list_view: 1,
-                            columns: 1,
-                            read_only: 1
-                        },
-                        {
-                            fieldtype: 'Button',
-                            fieldname: 'use',
-                            label: __('Use'),
-                            in_list_view: 1,
-                            columns: 1
-                        }
-                    ],
-                    data: selling_details,
-                    in_place_edit: false,
-                    get_data: function () {
-                        return selling_details;
-                    }
+                    fieldtype: 'HTML',
+                    fieldname: 'selling_history_table'
                 }
             ],
             primary_action_label: "Close",
@@ -88,50 +22,70 @@ frappe.ui.form.on('Sales Order Item', {
                 dialog.hide();
             }
         });
+        dialog.show();
 
         frappe.call({
-            method: "sales_history_app.custom_script.get_last_5_sales",
+            method: "spokes_service_app.custom_script.get_last_5_sales",
             args: { item_code: item_code },
             callback: function (r) {
                 if (r.message) {
-                    selling_details.length = 0;
+                    let table_html = `
+                        <div style="max-height: 400px; overflow-y: auto;">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>Inv No</th>
+                                        <th>Date</th>
+                                        <th>Customer</th>
+                                        <th>Rate</th>
+                                        <th>Qty</th>
+                                        <th>Amount</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                    `;
+
                     r.message.forEach(function (d) {
-                        selling_details.push({
-                            invoice_no: d.name,
-                            invoice_date: d.posting_date,
-                            customer_code: d.customer,
-                            rate: d.rate,
-                            qty: d.qty,
-                            amount: d.amount
-                        });
+                        let formatted_date = formatDate(d.posting_date);
+                        table_html += `
+                            <tr>
+                                <td>${d.name}</td>
+                                <td>${formatted_date}</td>
+                                <td>${d.customer}</td>
+                                <td>${d.rate}</td>
+                                <td>${d.qty}</td>
+                                <td>${d.amount}</td>
+                                <td><button class="btn btn-primary use-rate" data-rate="${d.rate}">Use</button></td>
+                            </tr>
+                        `;
                     });
 
-                    dialog.fields_dict.selling_details.grid.refresh();
+                    table_html += `</tbody></table></div>`;
+
+                    dialog.fields_dict.selling_history_table.$wrapper.html(table_html);
                     setup_use_button_event(dialog, frm, cdt, cdn);
                 }
             }
         });
-
-        dialog.show();
     }
 });
 
 function setup_use_button_event(dialog, frm, cdt, cdn) {
-    dialog.fields_dict.selling_details.grid.wrapper[0].addEventListener('click', function (event) {
-        var $target = $(event.target);
-        var $row = $target.closest('.grid-row');
-        var grid_row = $row.data('grid_row');
+    dialog.fields_dict.selling_history_table.$wrapper.on('click', '.use-rate', function () {
+        var selected_rate = $(this).data('rate');
+        var item_code = frappe.model.get_value(cdt, cdn, 'item_code');
 
-        if (grid_row && grid_row.doc && $target.attr('data-fieldname') === 'use') {
-            var selected_rate = grid_row.doc.rate;
-            var item_code = frappe.model.get_value(cdt, cdn, 'item_code');
-            frappe.model.set_value(cdt, cdn, 'price_list_rate', selected_rate);
-            frappe.model.set_value(cdt, cdn, 'rate', selected_rate);
+        frappe.model.set_value(cdt, cdn, 'price_list_rate', selected_rate);
+        frappe.model.set_value(cdt, cdn, 'rate', selected_rate);
 
-            frappe.msgprint(__('Rate {0} has been set for Item {1}', [selected_rate, item_code]));
+        frappe.msgprint(__('Rate {0} has been set for Item {1}', [selected_rate, item_code]));
 
-            frm.refresh_field('items');
-            dialog.hide();
-        }
-    }, true);
+        frm.refresh_field('items');
+        dialog.hide();
+    });
+}
+
+function formatDate(dateString) {
+    return dateString.split('-').reverse().join('-');
 }
